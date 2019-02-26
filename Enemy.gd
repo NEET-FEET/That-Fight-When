ed27a4f13f1486197de0d63cda014741940a1fb2
NEET-FEET <NEET-FEET@pm.me>
2019-02-26 06:13:53 +0100

    Added /v/ rage character
    currently has exactly the same nodes and script as the player, rushed it in so /v/ would stop sperging.

---
+++ Enemy.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 30
+const SPEED = 350
+const JUMP_HEIGHT = -500
+const ATTACK_TIME = 7
+
+var motion = Vector2()
+var attack = 0
+var left = true
+
+func _physics_process(delta):
+	motion.y += GRAVITY
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		left = true
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		left = false
+	else:
+		motion.x = 0
+	
+	if Input.is_action_pressed("ui_select"):
+		get_node("AnimatedSprite").play("attack")
+		attack = ATTACK_TIME
+	else:
+		if attack == 0:
+			get_node("AnimatedSprite").play("default")
+		else:
+			attack -= 1
+	
+	
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	
+	get_node("AnimatedSprite").set_flip_h(left)
+	motion = move_and_slide(motion, UP)
