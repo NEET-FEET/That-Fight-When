94df17aa1b5510aa94b59c6d40540b7d4c0bd4e8
NEET-FEET <NEET-FEET@pm.me>
2019-02-26 05:32:37 +0100

    made the player sprite reflect the move direction

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -8,19 +8,21 @@ const ATTACK_TIME = 7
 
 var motion = Vector2()
 var attack = 0
+var left = false
 
 func _physics_process(delta):
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		left = false
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		left = true
 	else:
 		motion.x = 0
 	
 	if Input.is_action_pressed("ui_select"):
-		print("select")
 		get_node("AnimatedSprite").play("attack")
 		attack = ATTACK_TIME
 	else:
@@ -28,7 +30,11 @@ func _physics_process(delta):
 			get_node("AnimatedSprite").play("default")
 		else:
 			attack -= 1
+	
+	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+	
+	get_node("AnimatedSprite").set_flip_h(left)
 	motion = move_and_slide(motion, UP)
