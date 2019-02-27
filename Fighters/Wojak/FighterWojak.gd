44bfe494778c6eb5ede655b4e21d3cd49f998532
AWK <arthurwk1800@gmail.com>
2019-02-27 18:34:53 -0300

    Removed unused input_id var

--- Fighters/Wojak/FighterWojak.gd
+++ Fighters/Wojak/FighterWojak.gd
@@ -1,6 +1,4 @@
 extends "res://Fighters/FighterBase.gd"
 
-var input_id = 0
-
 const SPEED = 250
 
--- Fighters/vRage/FightervRage.gd
+++ Fighters/vRage/FightervRage.gd
@@ -1,6 +1,4 @@
 extends "res://Fighters/FighterBase.gd"
 
-var input_id = 0
-
 const SPEED = 450
 const ATTACK_TIME = 0.05
